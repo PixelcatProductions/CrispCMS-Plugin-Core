351c5f08a54d01218a468e397713cfca36409d48
JustinBack <jback@pixelcatproductions.net>
2020-12-20 16:58:45 +0100

    Add featured browser beta

--- includes/views/downloads.php
+++ includes/views/downloads.php
@@ -1,5 +1,13 @@
 <?php
 
-const TEMPLATE_VARIABLES = array(
-    "variable1" => "I am a Var"
-);
+$FeaturedExtension;
+
+if (CURRENT_UNIVERSE >= crisp\Universe::UNIVERSE_BETA) {
+    foreach ($this->getConfig("extensions") as $Extension) {
+        if (strpos($Extension->browser, get_browser(null, true)["browser"]) !== false) {
+            $FeaturedExtension = $Extension;
+        }
+    }
+}
+
+$_vars["featured"] = $FeaturedExtension;
