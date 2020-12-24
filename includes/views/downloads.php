49dc5cfeb315e35413424c66a084d4e5bfa294ac
JustinBack <jback@pixelcatproductions.net>
2020-12-24 01:52:49 +0100

    Moved featured extension out of beta

--- includes/views/downloads.php
+++ includes/views/downloads.php
@@ -4,7 +4,7 @@ $FeaturedExtension;
 
 $_vars["extensions"] = $this->getConfig("extensions");
 
-if (CURRENT_UNIVERSE >= crisp\Universe::UNIVERSE_BETA) {
+if (CURRENT_UNIVERSE >= crisp\Universe::UNIVERSE_PUBLIC) {
     foreach ($_vars["extensions"] as $Key => $Extension) {
         if (strpos($Extension->browser, get_browser(null, true)["browser"]) !== false) {
             $FeaturedExtension = $Extension;
