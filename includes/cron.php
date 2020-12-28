4eaf9af04812b1488cc32049c48ab0c93e919deb
JustinBack <jback@pixelcatproductions.net>
2020-12-28 18:37:52 +0100

    Fix cron

--- includes/cron.php
+++ includes/cron.php
@@ -6,12 +6,9 @@ if (isset($_CRON)) {
     switch ($_CRON["Data"]->name) {
         case "cleanup_badges":
             $files = glob(__DIR__ . "/../../../api/badges/*.{png,svg}", GLOB_BRACE);
-            foreach($files as $file){
+            foreach ($files as $file) {
                 unlink($file);
             }
             break;
     }
-
-    \crisp\api\lists\Cron::markAsFinished($_CRON["ID"]);
-    exit;
 }
