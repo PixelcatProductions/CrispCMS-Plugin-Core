87e158fcf5a4344e5295e287d78612053e7710ce
JustinBack <jback@pixelcatproductions.net>
2021-01-17 00:37:34 +0100

    Added PGP Keys and Emails, updated edge icon. Fixes https://github.com/tosdr/CrispCMS/issues/60

--- includes/views/downloads.php
+++ includes/views/downloads.php
@@ -5,12 +5,12 @@ $FeaturedExtension;
 $_vars["extensions"] = $this->getConfig("extensions");
 
 if (CURRENT_UNIVERSE >= crisp\Universe::UNIVERSE_PUBLIC) {
-    foreach ($_vars["extensions"] as $Key => $Extension) {
-        if (strpos($Extension->browser, get_browser(null, true)["browser"]) !== false) {
-            $FeaturedExtension = $Extension;
-            unset($_vars["extensions"][$Key]);
-        }
+  foreach ($_vars["extensions"] as $Key => $Extension) {
+    if (strpos($Extension->browser, get_browser(null, true)["browser"]) !== false) {
+      $FeaturedExtension = $Extension;
+      unset($_vars["extensions"][$Key]);
     }
+  }
 }
 
 $_vars["featured"] = $FeaturedExtension;
